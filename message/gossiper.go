8ff1233ff311046af9cf8b1257746dfdc885f907
Valentin Quelquejay <valentin.quelquejay-leclere@epfl.ch>
2019-10-09 15:30:55 +0200

    rumor message struct

--- message/gossiper.go
+++ message/gossiper.go
@@ -9,6 +9,26 @@ type SimpleMessage struct {
 	Contents      string
 }
 
+//RumorMessage represents a type of Peerster message to be gossiped.
+type RumorMessage struct {
+	Origin string
+	ID     uint32
+	Text   string
+}
+
+//PeerStatus gives the next message ID to be received by a particular peer.
+type PeerStatus struct {
+	Identifier string
+	NextID     uint32
+}
+
+//StatusMessage is the type of message sent by a particular peer when receiving a message used
+// to acknowledge all the messages it has received so far.
+type StatusMessage struct {
+	Want []PeerStatus
+}
+
+//NewSimpleMessage creates a new simpleMessage.
 func NewSimpleMessage(contents string, originalName string, relayPeerAddr string) *SimpleMessage {
 	return &SimpleMessage{
 		OriginalName:  originalName,
@@ -17,8 +37,36 @@ func NewSimpleMessage(contents string, originalName string, relayPeerAddr string
 	}
 }
 
-//Print simple message
+//NewRumorMessage creates a new rumorMessage.
+func NewRumorMessage(origin string, ID uint32, text string) *RumorMessage {
+	return &RumorMessage{
+		Origin: origin,
+		ID:     ID,
+		Text:   text,
+	}
+}
+
+//Prints simpleMessage.
 func (msg *SimpleMessage) String() string {
 	return fmt.Sprintf("SIMPLE MESSAGE origin %s from %s contents %s \n", msg.OriginalName,
 		msg.RelayPeerAddr, msg.Contents)
+}
+
+//Prints a RumorMessage
+func (msg *RumorMessage) String() string {
+	return fmt.Sprintf("RUMOR MESSAGE origin %s with ID %s contents %s \n", msg.Origin, msg.ID, msg.Text)
+}
+
+//Prints a PeerStatus message
+func (ps *PeerStatus) String() string {
+	return fmt.Sprintf("Identifier : %s, NextID : %u \n", ps.Identifier, ps.NextID)
+}
+
+//Prints a StatusMessage
+func (msg *StatusMessage) String() string {
+	str := fmt.Sprintf("STATUS MESSAGE received \n")
+	for _, element := range msg.Want {
+		str += fmt.Sprintf("%s \n", element.String())
+	}
+	return str
 }
